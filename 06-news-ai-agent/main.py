ff5dd07437e1f5dfad0dedc88554d01af1bf33c1
Gaurang Saini <gaurangsaini01@gmail.com>
2025-06-04 20:24:45 +0530

    Updating AI prompt to give better results

--- 06-news-ai-agent/main.py
+++ 06-news-ai-agent/main.py
@@ -15,12 +15,11 @@ def get_news(topic,category,ps=2):
                                           language='en',
                                           country='us',
                                           page=1,page_size=ps)
-    print(top_headlines)
     news_list = []
     for a in top_headlines["articles"]:
-        # print(f"{count}: {a.get('title')}")
-        news = a["url"]
-        news_list.append(news)
+        news_url = a["url"]
+        news_title = a["title"]
+        news_list.append({"title":news_title,"url":news_url})
 
     return news_list
         
@@ -57,8 +56,25 @@ SYSTEM_PROMPT = f"""
 
     Output: {{"step":"action","function":"get_news","topic":"AI","ps":"2","category":"technology"}}
 
-    Output: {{"step":"observe","output":"1: https://url1.com\n2: https://url2.com\n3: https://url3.com"}}
-    Output: {{"step":"output","content":"1: https://url1.com\n2: https://url2.com\n3: https://url3.com"}}
+    Output: {{"step":"observe","output":"1:Title:"AI is getting scary" \n URL:https://url1.com\n2:Title:"AI gets even better with agents", url: https://url2.com\n"}}
+    Output: {{"step":"output","content":"1:Title:"AI is getting scary" \n URL:https://url1.com\n2:Title:"AI gets even better with agents", url: https://url2.com\n"}}
+
+    
+    Example:
+    User: news on top android updates?
+    Output: {{"step":"start","content":"So I have to get news related to top android updates, I will check my available tools if there is any"}}
+    Output: {{"step":"plan","content":"I found an available tool "get_news" that can help me get the news"}}
+
+    #in the action step you yourself decide which category the users query belong to , you can only choose from following categories : business, entertainment, general, health, science, sports ,technology .
+    #you can only choose one category from the given one's , dont use any other category.
+
+    #in case user provides long topics like "top android updates" you can make it "android" and search news related to it instead of nothing
+
+    Output: {{"step":"action","function":"get_news","topic":"android","ps":"2","category":"technology"}}
+
+    Output: {{"step":"observe","output":"1:Title:"Android phones getting cheaper" \n URL:https://url1.com\n2:Title:"android beats apple, gets even better with ai", url: https://url2.com\n"}}
+    Output: {{"step":"output","content":"1:Title:"Android phones getting cheaper" \n URL:https://url1.com\n2:Title:"android beats apple, gets even better with ai", url: https://url2.com\n"}}
+
 
 
     Keep in mind you must not exceed these 5 steps . Only these 5 steps must be there . If no result found , simply say "No News Found"
@@ -72,19 +88,20 @@ messages = [{"role":"system","content":SYSTEM_PROMPT}]
 query = input("User: ")
 messages.append({"role": "user", "content": query})
 
+
 while True:
     response = client.chat.completions.create(
-        model = "gpt-4o-mini",
+        model = "gpt-4.1",
         response_format={"type":"json_object"},
         messages = messages
     )
     messages.append({"role":"assistant","content":response.choices[0].message.content})
     parsed = json.loads(response.choices[0].message.content)
 
-    step = parsed.get("step")
+    step = parsed.get("step")   
     content = parsed.get("content")
 
-    # print(messages,"\n")
+    print(messages,"\n")
 
     if step == "action" :
         news_list = tools[parsed.get("function")](parsed.get("topic"), parsed.get("category"),int(parsed.get("ps")))
