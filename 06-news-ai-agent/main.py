916f5aef5a2b219748c5bdeeb300de8259a336f2
Gaurang Saini <gaurangsaini01@gmail.com>
2025-06-04 20:08:19 +0530

    News Agent updated to send Links now

--- 06-news-ai-agent/main.py
+++ 06-news-ai-agent/main.py
@@ -9,24 +9,24 @@ client = OpenAI()
 newsapi = NewsApiClient(api_key=os.getenv("NEWS_API_KEY"))
 
 
-def get_weather(city):
-    return "12 degree celsius"
-
-def get_news(topic,ps):
+def get_news(topic,category,ps=2):
     top_headlines = newsapi.get_top_headlines(q=topic,
-                                          category="technology",
+                                          category=category,
                                           language='en',
                                           country='us',
                                           page=1,page_size=ps)
-    # print(top_headlines)
-    count = 1
+    print(top_headlines)
+    news_list = []
     for a in top_headlines["articles"]:
-        print(f"{count}: {a.get('title')}")
-        count = count+1
+        # print(f"{count}: {a.get('title')}")
+        news = a["url"]
+        news_list.append(news)
+
+    return news_list
+        
 
 
 tools = {
-    "get_weather":get_weather,
     "get_news":get_news
 }
 
@@ -39,8 +39,7 @@ SYSTEM_PROMPT = f"""
     and upon tool selection you perform an action to call the tool
 
     Available Tools:
-    1. "get_weather": takes a city name as input and return the temperature of that city
-    2. "get_dancing" : dances for you
+    1. "get_news": Fetches the top news for the requested topic .
 
     Rules:
     1. Strictly give output in JSON format
@@ -49,25 +48,22 @@ SYSTEM_PROMPT = f"""
 
     Example:
 
-    User: What's the weather in Patiala?
-    Output: {{"step":"start","content":"So I have to get the weather of patiala , I will check my available tools if there is any"}}
-    Output: {{"step":"plan","content":"I found an available tool "get_weather" that can help me get the weather"}}
-    Output: {{"step":"action","function":"get_weather","input":"patiala"}}
-    Output: {{"step":"observe","output":"12 degree cel"}}
-    Output: {{"step":"output","content":"The weather of patiala is 12 degrees ."}}
-
-    User: Provide me 3 news about AI ?
-    Output: {{"step":"start","content":"So I have to get 3 news related to AI, I will check my available tools if there is any"}}
+    User: Provide me 2 news about AI ?
+    Output: {{"step":"start","content":"So I have to get 2 news related to AI, I will check my available tools if there is any"}}
     Output: {{"step":"plan","content":"I found an available tool "get_news" that can help me get the news"}}
-    Output: {{"step":"action","function":"get_news","topic":"AI","ps":"3"}}
 
-    Output: {{"step":"observe","output":"1:Meta and Yandex are de-anonymizing Android users web browsing identifiers - Ars Technica
-            2:Nintendo Prepping For All Switch 2 Eventualities With 'Out Of Stock' Signs - Nintendo Life
-            3:I ignored Google's passkey prompts for months, but now I feel silly for waiting - Android Authority"}}
-    Output: {{"step":"output","content":"1:Meta and Yandex are de-anonymizing Android users web browsing identifiers - Ars Technica
-            2:Nintendo Prepping For All Switch 2 Eventualities With 'Out Of Stock' Signs - Nintendo Life
-            3:I ignored Google's passkey prompts for months, but now I feel silly for waiting - Android Authority"}}
+    #in the action step you yourself decide which category the users query belong to , you can only choose from following categories : business, entertainment, general, health, science, sports ,technology .
+    #you can only choose one category from the given one's , dont use any other category.
 
+    Output: {{"step":"action","function":"get_news","topic":"AI","ps":"2","category":"technology"}}
+
+    Output: {{"step":"observe","output":"1: https://url1.com\n2: https://url2.com\n3: https://url3.com"}}
+    Output: {{"step":"output","content":"1: https://url1.com\n2: https://url2.com\n3: https://url3.com"}}
+
+
+    Keep in mind you must not exceed these 5 steps . Only these 5 steps must be there . If no result found , simply say "No News Found"
+
+        
 """
 
 
@@ -84,14 +80,15 @@ while True:
     )
     messages.append({"role":"assistant","content":response.choices[0].message.content})
     parsed = json.loads(response.choices[0].message.content)
+
     step = parsed.get("step")
     content = parsed.get("content")
 
-  
+    # print(messages,"\n")
 
     if step == "action" :
-        ans = tools[parsed.get("function")](parsed.get("topic"),int(parsed.get("ps")))
-        messages.append({"role":"assistant","content":json.dumps({"step":"observe","output":ans})})
+        news_list = tools[parsed.get("function")](parsed.get("topic"), parsed.get("category"),int(parsed.get("ps")))
+        messages.append({"role":"assistant","content":json.dumps({"step":"observe","output":news_list})})
 
     if step == "output" :
         print(content)
